c748715e1b21c26af80ab6d913f7237ee4f6b355
kelengkeng <72908892+kelengkeng@users.noreply.github.com>
2021-07-16 00:00:50 +0700

    update

--- tortoolkit/core/status/upload.py
+++ tortoolkit/core/status/upload.py
@@ -75,7 +75,7 @@ class TGUploadTask(Status):
         self._current_file = str(name)
 
     async def create_message(self):
-        msg = "<b>Uploading:- </b> <code>{}</code>\n".format(
+        msg = "🔼 <code>{}</code>\n".format(
             self._current_file
         )
         prg = 0
@@ -83,15 +83,15 @@ class TGUploadTask(Status):
             prg = self._uploaded_files/self._files
 
         except ZeroDivisionError:pass
-        msg += "<b>Progress:- </b> {} - {}%\n".format(
+        msg += "🌀 <code>{} {}%</code>\n".format(
             self.progress_bar(prg),
             prg*100
         )
-        msg += "<b>Files:- </b> {} of {} done.\n".format(
+        msg += "📦 <code>{}</code> of <code>{} done.</code>\n".format(
             self._uploaded_files,
             self._files
         )
-        msg += "<b>Using Engine:- </b> <code>TG Upload</code>\n"
+        msg += "⚙️ <code>TG Upload</code>\n"
         return msg
 
     def progress_bar(self, percentage):
@@ -144,8 +144,8 @@ class RCUploadTask(Status):
         nstr = nstr.strip()
         nstr = nstr.split(",")
         prg = nstr[1].strip("% ")
-        prg = "Progress:- {} - {}%".format(self.progress_bar(prg),prg)
-        progress = "<b>Uploaded:- {} \n{} \nSpeed:- {} \nETA:- {}</b> \n<b>Using Engine:- </b><code>RCLONE</code>".format(nstr[0],prg,nstr[2],nstr[3].replace("ETA",""))
+        prg = "🌀 <code>{} {}%</code>".format(self.progress_bar(prg),prg)
+        progress = "📦 <code>{}</code> \n<code>{}</code> \n⚡️ <code>{}</code> \n⏱ </code>{}</code> \n⚙️ <code>RCLONE</code>".format(nstr[0],prg,nstr[2],nstr[3].replace("ETA",""))
         return progress
 
     def progress_bar(self,percentage):
